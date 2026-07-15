541787f4181c0844f646c8a7cd6f02e6cf4e393c
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-07-15 20:54:52 +0200

    Re-add Nix flake

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Builds the T4EQ website using hugo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forEachSystem =
+        fn:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          fn pkgs
+        );
+
+      buildHugoPackage =
+        {
+          pkgs,
+          pname,
+          version,
+          src,
+          extraBuildArgs ? "",
+        }:
+        (pkgs.stdenvNoCC.mkDerivation {
+          inherit pname version src;
+
+          nativeBuildInputs = with pkgs; [
+            hugo
+          ];
+
+          buildPhase = ''
+            hugo ${extraBuildArgs}
+          '';
+
+          installPhase = ''
+            cp -r public $out
+          '';
+        });
+
+    in
+    {
+      packages = forEachSystem (pkgs: rec {
+        t4eq = buildHugoPackage {
+          inherit pkgs;
+          pname = "t4eq";
+          version = "1.0.0";
+          src = self;
+          extraBuildArgs = "--minify";
+        };
+        default = t4eq;
+      });
+
+      devShells = forEachSystem (pkgs: rec {
+        t4eq = pkgs.mkShellNoCC {
+          nativeBuildInputs = with pkgs; [
+            hugo
+          ];
+        };
+
+        default = t4eq;
+      });
+    };
+}
